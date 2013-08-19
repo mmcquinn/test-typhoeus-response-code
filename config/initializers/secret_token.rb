a7d080523bd481ad63981044e4e6a67c9085f537
Michael McQuinn <mike@mcquinntechnologies.com>
2013-08-19 18:10:22 -0400

    Initial put, AO

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestTyphoeus::Application.config.secret_token = 'e94adfc56cd9b08e62f56dea1645b90749a32688bbbc5b98bf47ddf5bae6c60b58fb1a8e13ed2bb3be09d252b5d320ef62ca14bb89296aa3852ed0ffcc1d51d7'
